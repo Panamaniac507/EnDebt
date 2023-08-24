1c4a497b945849b9370d07fb93af19b13ac7efb8
ryo nakano <oryom17r15@gmail.com>
2023-08-24 21:52:57 +0900

    make confetti work

--- app/javascript/controllers/confetti_controller.js
+++ app/javascript/controllers/confetti_controller.js
@@ -5,13 +5,21 @@ export default class extends Controller {
   connect() {
     console.log("confetti")
     // this.particles = new particlesJS()
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const payment = urlParams.get('payment')
+
+    if (payment) {
+      this.celebrate()
+    }
   }
   celebrate(event) {
-    event.preventDefault()
+    // event.preventDefault()
     console.log("celebrate")
     particlesJS.load('particles-js', "pconfig.json", function() {
     setTimeout(() => {
-      document.querySelector("#particles-js").remove()
+      this.element.remove()
     }, 3000);
       console.log('callback - particles-js config loaded');
     });
